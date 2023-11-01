4be770cc0fabfd9ff48a407f5a14af47c67b0e8b
Soul-Xu <741930067@qq.com>
2023-11-01 14:32:42 +0800

    feat: 调整home页显示

--- nest-next/src/client/pages/home/index.tsx
+++ nest-next/src/client/pages/home/index.tsx
@@ -25,12 +25,12 @@ const Home: NextPage = () => {
         <h1 className={classNames("container-title")}>事件管理</h1>
         <div className={classNames("container-content")}>
           <BasicInformation />
-          {/* <HandleInformation />
+          <HandleInformation />
           <ReasonAnalysis />
           <RecoveryProcess />
           <EffectAnalysis />
           <AttachmentUpload />
-          <SubmitElement /> */}
+          <SubmitElement />
         </div>
       </section>
     </PageLayout>
